e440766b64ffb49df2ad1fce8ce9bea5267f6a36
brandiux <brandon.valenzuela247077@potros.itson.edu.mx>
2025-03-28 21:57:35 -0700

    Proyecto en 4 capas Persistencia, Dominio, Negocio y Presentación

---
+++ NegocioComandas/src/main/java/com/mycompany/negociocomandas/NegocioComandas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.negociocomandas;
+
+/**
+ *
+ * @author brand
+ */
+public class NegocioComandas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ PersistenciaComandas/src/main/java/com/mycompany/persistenciacomandas/PersistenciaComandas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.persistenciacomandas;
+
+/**
+ *
+ * @author brand
+ */
+public class PersistenciaComandas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
